d87f9af1e220aaa4e75809b73d3ed341a0130bf9
Nirmal-72 <nirmalk8009@gmail.com>
2025-05-22 22:33:14 +0530

    Delete Shop directory

--- Shop/shop.js
+++
@@ -1,9 +0,0 @@
-let cartCount = 0;
-let wishCount = 0;
-
-document.querySelectorAll('.product-card button').forEach(btn => {
-  btn.addEventListener('click', () => {
-    cartCount++;
-    document.getElementById('cart-count').innerText = cartCount;
-  });
-});
